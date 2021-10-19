2db8a9d8b0ac590781e0428fcc211b42346322cd
william <williamchan_7@163.com>
2021-10-19 11:14:54 +0800

    second commit

--- src/main/java/com/william/git/GitTest.java
+++ src/main/java/com/william/git/GitTest.java
@@ -2,6 +2,7 @@ package com.william.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello Git!");
+        System.out.println("hello Git1!");
+        System.out.println("hello Git2!");
     }
 }
